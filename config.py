c513dadfa8d52ad7c33a93af03094cb4ff887fde
saraansh rawat <42444561+snickrscodes@users.noreply.github.com>
2026-09-02 19:27:05 -0400

    Refactor Config class with new parameters
    Updated configuration parameters for model training and evaluation.

--- config.py
+++ config.py
@@ -1,30 +1,36 @@
 from dataclasses import dataclass
+from typing import Literal
+
 
 @dataclass
 class Config:
+    model_name: Literal["cnn", "p4m"] = "cnn"
     seed: int = 0
-    horizon_len: int = 512
+    eval_seed: int = 100003
+
+    horizon_len: int = 128
     n_rollouts_phase: int = 32
     n_phases: int = 200
-    n_envs_worker: int = 256
+    n_envs_worker: int = 64
+    n_envs_test: int = 64
     n_eps_test: int = 1
     max_placements_test: int = 3000
 
     n_epochs_V: int = 1
-    n_hands_V: int = 512
-    
+    n_hands_V: int = 64
+
     ent_coef_P: float = 0.0
-    P_beam_width: int = 576
-    P_per_parent_top_m: int = 24
+    P_beam_width: int = 128
+    P_per_parent_top_m: int = 16
     P_teacher_tau: float = 2.0
     P_root_eps: float = 0.5
-    
+
     V_gamma: float = 0.997
     V_lmbda: float = 0.99
 
     max_bs_train: int = 2048
-    max_bs_inference_P: int = 42000
-    max_bs_inference_V: int = 44000
+    max_bs_inference_P: int = 40000
+    max_bs_inference_V: int = 100000
 
     base_lr_P: float = 5e-4
     base_lr_V: float = 3e-4
@@ -40,3 +46,6 @@ class Config:
 
     P_grad_accum_steps: int = 1
     V_grad_accum_steps: int = 1
+
+    warmup_steps_P: int = 0
+    warmup_steps_V: int = 0
